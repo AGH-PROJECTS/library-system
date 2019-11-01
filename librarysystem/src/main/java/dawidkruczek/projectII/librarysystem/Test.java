56e8244fda652ae32ddfb71b0f7144f2b5a9ae20
Rozamer <dawid.kruczek.1997@gmail.com>
2019-11-01 14:54:24 +0100

    Test commit

--- librarysystem/src/main/java/dawidkruczek/projectII/librarysystem/Test.java
+++ librarysystem/src/main/java/dawidkruczek/projectII/librarysystem/Test.java
@@ -1,8 +1,11 @@
+package dawidkruczek.projectII.librarysystem;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test {
     public static void main(String[] args) {
-
+        SpringApplication.run(Test.class);
     }
 }
